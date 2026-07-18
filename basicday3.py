745f0d7044c17d32f69123f6ba683eeea8dc7e88
bunny-janga <bunnyjanga.ai@gmail.com>
2026-07-18 12:25:16 +0530

    Add files via upload

---
+++ basicday3.py
@@ -0,0 +1,17 @@
+details = {
+    'name':'sandeep',
+    'college':"gvp",
+    'pan': 546446,
+    'adhr':6876,
+    'pin': 1234
+}
+print(details.keys())
+print(details.values())
+print(details.items())
+details.update({'name':'koviri sandeep'})
+details.update({'gender':'male'})
+print(details)
+details['name'] ='sandeepkoviri'
+# details.clear()
+print(details)
+print(details['name'])#to get
